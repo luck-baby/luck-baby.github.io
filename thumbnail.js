c797a18d59dd962b25a437743848469314cea37d
luck-baby <101320418+luck-baby@users.noreply.github.com>
2022-03-19 23:04:14 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -4,6 +4,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
     let targetImage = document.querySelector("#smart-image");
     targetImage.addEventListener("click", function() {
         alert("点击了图片");
-    targetImage.classList.remove("small");
 });
+    targetImage.classList.remove("small");
 });
