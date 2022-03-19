db185cac37af4c53957dcdee6384903ae8baf771
luck-baby <101320418+luck-baby@users.noreply.github.com>
2022-03-19 22:28:28 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -3,6 +3,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
     alert("Hello JavaScript");
     let targetImage = document.querySelector("#smart-image");
     targetImage.addEventListener("click", function() {
-    alert("点击了图片");
+        alert("点击了图片");
 });
 });
