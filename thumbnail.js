a9fdff1cda6ab3ce7cb299887b00b59866f92872
luck-baby <101320418+luck-baby@users.noreply.github.com>
2022-03-19 21:49:23 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,7 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     // 你的所有代码都应该写在这里
     alert("Hello JavaScript");
-    targetImage.addEventListener("click", function() {
-    alert("点击了图片");
-//});
+   
 });
